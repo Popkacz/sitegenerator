5a2a75d272577eb31127ca63fe01d0605cd25301
Popkacz <mendesguilherme073@gmail.com>
2024-06-26 16:38:11 -0300

    Starting project

---
+++ src/htmlnode.py
@@ -0,0 +1,16 @@
+class HTMLNode:
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self): 
+
+        prop_str = ""
+
+        for prop in self.props:
+            
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    first_test = TextNode('This is a text node', "bold", "https://www.boot.dev")
+    first_test.__repr__()
+
+
+main()
